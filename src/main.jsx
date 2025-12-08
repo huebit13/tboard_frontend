e320b64ea60d8c1fc697ffcdc1568cf2f2c3d380
huebit13 <antipin2005050514@gmail.com>
2025-12-08 15:58:27 +0300

    fix: working Vercel deployment with TON + Buffer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,3 @@
-import { Buffer } from 'buffer'
-import process from 'process'
-
-window.Buffer = Buffer
-window.process = process
-window.global = globalThis
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [react()],
-
-  resolve: {
-    alias: {
-      buffer: 'buffer/',
-      process: 'process/browser',
-      stream: 'stream-browserify',
-      util: 'util/',
-      crypto: 'crypto-browserify',
-      assert: 'assert/',
-      http: 'stream-http',
-      https: 'https-browserify',
-      os: 'os-browserify',
-      url: 'url/',
-      path: 'path-browserify',
-    }
-  },
-
-  define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    global: 'globalThis',
-  },
+  plugins: [
+    react(),
+    nodePolyfills({
+      buffer: true,
+      process: true,
+      global: true,
+    })
+  ],
 })
