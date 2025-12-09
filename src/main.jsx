30526592ff151f70be6c8d51804fc0e5ca6ac663
huebit13 <antipin2005050514@gmail.com>
2025-12-09 11:29:46 +0300

    reoptimized frontend

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { TonConnectProvider } from './TonConnectProvider.jsx'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
   <StrictMode>
     <TonConnectProvider>
       <App />
